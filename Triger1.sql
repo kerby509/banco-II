c72d967667fb8485b1b71c35440ab666c126069c
kerby <lovincekerby99@gmail.com>
2023-09-09 21:57:45 -0300

    triger_1

--- Triger1.sql
+++ Triger1.sql
@@ -6,3 +6,19 @@ CREATE TABLE historico_salarios (
     salario_antigo int NOT NULL,
     salario_novo int NOT NULL
 );
+
+CREATE OR REPLACE FUNCTION registrar_historico_salario()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF OLD.salario <> NEW.salario THEN
+        INSERT INTO historico_salarios (emp_id, usuario_alteracao, salario_antigo, salario_novo)
+        VALUES (NEW.emp_id, current_user, OLD.salario, NEW.salario);
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER salario_alterado
+AFTER UPDATE ON empregados
+FOR EACH ROW
+EXECUTE FUNCTION registrar_historico_salario();
