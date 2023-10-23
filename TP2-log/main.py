789e585f4e659dac364100a2bc4b23a400c6bb2f
kerby <lovincekerby99@gmail.com>
2023-10-23 19:52:08 -0300

    trabalho log

--- TP2-log/main.py
+++ TP2-log/main.py
@@ -1,9 +1,16 @@
 import psycopg2 
 from copy import copy
+import json
 
 #inicia a simulação
 def getLog (name, con):
     createTable(con)
+    file = open(name, "r")
+    data = file.readlines()
+    data = data[data.index("\n")+1:]
+    data.reverse()
+    file.close()
+    return data
 
 
 
